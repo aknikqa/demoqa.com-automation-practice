c7ec16f792c7eb3f8203ca7eac360718cff3bbd4
aknik <aknikqa@gmail.com>
2023-10-28 17:08:51 +0300

    TextBoxTests

---
+++ src/test/java/Tests/TextBoxTests.java
@@ -0,0 +1,33 @@
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+
+    }
+    @Test
+    void FillFormtest() {
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("Nik");
+        $("#userEmail").setValue("nik@test.com");
+        $("#currentAddress").setValue("Some adress 1 ");
+        $("#permanentAddress").setValue("Other adress street");
+        $("#submit").pressEnter();
+
+        $("#output #name").shouldHave(text("Nik"));
+        $("#output #email").shouldHave(text("nik@test.com"));
+        $("#output #currentAddress").shouldHave(text("Some adress 1 "));
+        $("#output #permanentAddress").shouldHave(text("Other adress street"));
+
+    }
+}
---
+++ src/test/java/simpletestdemoqa.java
@@ -0,0 +1,16 @@
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+    public class simpletestdemoqa {
+        @Test
+        void simpletestdemoqa() {
+            open("https://www.google.com/");
+            $("button + button").click();
+            $("[name=q]").setValue("selenide").pressEnter();
+            $("[id=search]").shouldHave(text("selenide.org"));
+        }
+    }
