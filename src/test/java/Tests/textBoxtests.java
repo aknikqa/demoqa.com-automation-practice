2509ce303819e7790d48397bcf2b2a61e90bc2f6
aknik <aknikqa@gmail.com>
2023-11-03 11:14:43 +0300

    automationpracticeform

--- src/test/java/Tests/textBoxtests.java
+++ src/test/java/Tests/textBoxtests.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class TextBoxTests {
+public class textBoxtests {
 
     @BeforeAll
     static void beforeAll() {
