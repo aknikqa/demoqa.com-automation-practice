d5db84b2c051eccc8f5c237c4eee6031c1908d94
aknik <aknikqa@gmail.com>
2023-11-07 23:29:40 +0300

    wikiSoftAssertionsTest

--- src/test/java/tests/SelenideSearchTest.java
+++ src/test/java/tests/SelenideSearchTest.java
@@ -1,13 +1,8 @@
-
-
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.*;
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byLinkText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,6 +12,7 @@ public class SelenideSearchTest {
         Configuration.baseUrl = "https://github.com";
         Configuration.pollingInterval = 2000;
     }
+
     @Test
     void wikiSoftAssertionsTest() {
         // - Откройте страницу Selenide в Github
@@ -24,9 +20,8 @@ public class SelenideSearchTest {
         // - Перейдите в раздел Wiki проекта
         $("#wiki-tab").click();
         // - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
-       $x("//li[@class='Box-row wiki-more-pages-link'])");
-        $x("//span[@class='Truncate'])[18]").click();
-
+        $("button.js-wiki-more-pages-link").click();
+        $(byText("SoftAssertions")).click();
 
 
     }
